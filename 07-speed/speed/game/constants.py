e3afbb44969befc15760d366b15daf3a4f5a7d6a
Scott Burton <burtons@byui.edu>
2021-11-01 10:51:28 -0600

    Updated to reflect the Raylib input service, rather than AsciiMatics

--- 07-speed/speed/game/constants.py
+++ 07-speed/speed/game/constants.py
@@ -1,8 +1,17 @@
 import os
 
-MAX_X = 60
-MAX_Y = 20
-FRAME_LENGTH = 0.08
+MAX_X = 800
+MAX_Y = 400
+FRAME_RATE = 30
+
+DEFAULT_SQUARE_LENGTH = 20
+DEFAULT_FONT_SIZE = 20
+DEFAULT_TEXT_OFFSET = 5
+
+MAX_SPEED = 5
+WORD_GENERATION_RATE = 0.02
+
 STARTING_WORDS = 5
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
+
